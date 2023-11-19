e4510c1968f81653f1bae92f0d687ae7ac283a3c
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2023-11-19 14:12:42 +0800

    chrome chatbot extension demo

--- popup.js
+++ popup.js
@@ -1,29 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var conversation = document.getElementById("conversation");
-    var userInput = document.getElementById("user-input");
-    var submitButton = document.getElementById("submit-button");
-  
-    userInput.addEventListener("keyup", function(event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        submitButton.click();
-      }
-    });
-  
-    submitButton.addEventListener("click", function() {
-      var userMessage = userInput.value;
-      conversation.innerHTML += "<p>User: " + userMessage + "</p>";
-      userInput.value = "";
-    });
+const messages = document.getElementById("messages");
+const userInput = document.getElementById("user-input");
+const sendButton = document.getElementById("send-button");
 
-    function chat_with_chatbot(message) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://localhost:5000/chatbot", false);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify({ conversation: message }));
-        var response = JSON.parse(xhr.responseText);
-        return response.response;
-      }
-      
-  });
- 
+function addMessage(text, sender) {
+    const message = document.createElement("div");
+    message.className = sender;
+    message.textContent = text;
+    messages.appendChild(message);
+    messages.scrollTop = messages.scrollHeight;
+}
+
+function sendMessage() {
+
+    const userInputText = userInput.value.trim();
+
+    if (userInputText === 'csdn') {
+        addMessage(userInputText, "user");
+        addMessage("Open CSDN", "chatbot");
+        window.open('https://blog.csdn.net/universsky2015');
+    }
+
+    if (userInputText === 'weibo') {
+        addMessage(userInputText, "user");
+        addMessage("Open weibo", "chatbot");
+        window.open('https://weibo.com/');
+    }
+
+}
+
+sendButton.addEventListener("click", sendMessage);
+userInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
